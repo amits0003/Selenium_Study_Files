a2b4518f876b1e2319a376e3971a34698938abff
amits0003 <veddaily@gmail.com>
2021-08-04 15:32:23 +0530

    added source code files

---
+++ SeleniumTest/alerts/find_minimum_maximum_by_adding_exactly_four_out_of_five_integers_in_array.py
@@ -0,0 +1,9 @@
+arr = [1, 2, 3, 5, 6]
+arr1 = [4, 3, 2, 5, 3]
+min_list = []
+max_list = []
+temp = 0
+min_temp = []
+for index, value in enumerate(arr):
+    if value < arr[index + 1]:
+        min_temp.append()
