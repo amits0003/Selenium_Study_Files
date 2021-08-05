63c074c8831bb32f0777e3be736eb392ba040def
amits0003 <veddaily@gmail.com>
2021-08-05 13:23:06 +0530

    added source code files

--- SeleniumTest/alerts/find_minimum_maximum_by_adding_exactly_four_out_of_five_integers_in_array.py
+++ SeleniumTest/alerts/find_minimum_maximum_by_adding_exactly_four_out_of_five_integers_in_array.py
@@ -1,20 +1,85 @@
-arr = [1, 2, 3, 5, 6]
-arr1 = [4, 3, 2, 5, 99,-2]
+arr1 = [1, 2, 3, 5, 6]
 
+arr = [1,2,3,4,5]
+# --------------------approach 1----------------------------------------
+'''
 min_list = []
 max_list = []
-temp = 0
+max_value = 0
+min_value = 0
 min_temp = []
+sum_max_list = 0
+sum_min_list = 0
 
-for index, value in enumerate(arr1):
-    print(index)
+if len(arr) <= 5:
+    for index, value in enumerate(arr):
+        if value > max_value:
+            max_value = value
+    for index, value in enumerate(arr):
+        if value < arr[index-1]:
+            min_value = value
 
-    if value > temp:
-        temp = value
-    else:
-        min_list.append(value)
+    for ele in arr:
+        if ele< max_value:
+            min_list.append(ele)
+
+    for ele in arr:
+        if ele>min_value:
+            max_list.append(ele)
+
+    for ele in min_list:
+        sum_min_list = sum_min_list+ele
+
+    for ele in max_list:
+        sum_max_list = sum_max_list+ele
+
+    print(str(sum_min_list)+" "+str(sum_max_list))
 
 
+print('max value : ', max_value)
+print('min value : ', min_value)
+print('max value list : ', max_list)
+print('min value list :  ', min_list)
 
-print(min_list)
+'''
+# --------------------------------approach 2 ------------------------------------#
+
+max_value = 0
+min_value = 0
+list_of_sum_of_four_elements = []
+temp1, temp2, temp3, temp4, temp5 = 0, 0, 0, 0, 0
+if len(arr) <= 5:
+    for index, value in enumerate(arr):
+        print(index, value)
+        if index in [1,2,3,4] :
+            temp1 = temp1+value
+
+        if index in [0,2,3,4]:
+            temp2 = temp2+value
+
+        if index in [0,1,3,4]:
+            temp3 = temp3+value
+
+        if index in [0,1,2,4]:
+            temp4 = temp4+value
+
+        if index in [0,1,2,3]:
+            temp5 = temp5+value
+
+
+list_of_sum_of_four_elements.append(temp1)
+list_of_sum_of_four_elements.append(temp2)
+list_of_sum_of_four_elements.append(temp3)
+list_of_sum_of_four_elements.append(temp4)
+list_of_sum_of_four_elements.append(temp5)
+print(list_of_sum_of_four_elements)
+
+# for index, value in enumerate(list_of_sum_of_four_elements):
+#         if value > max_value:
+#             max_value = value
+# for index, value in enumerate(list_of_sum_of_four_elements):
+#         if value < list_of_sum_of_four_elements[-index]:
+#             min_value = value
+
+print(max(list_of_sum_of_four_elements), min(list_of_sum_of_four_elements))
 
