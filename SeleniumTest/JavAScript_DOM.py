e2d36291c8df5237318801da755209cf195a585f
amits0003 <veddaily@gmail.com>
2021-08-05 08:12:03 +0530

    added source code files

---
+++ SeleniumTest/JavAScript_DOM.py
@@ -0,0 +1,2 @@
+# javascript DOM can access any element in web age just like how selenium does
+# selenium have a method to execute javascript code in it
--- SeleniumTest/alerts/find_minimum_maximum_by_adding_exactly_four_out_of_five_integers_in_array.py
+++ SeleniumTest/alerts/find_minimum_maximum_by_adding_exactly_four_out_of_five_integers_in_array.py
@@ -1,9 +1,20 @@
 arr = [1, 2, 3, 5, 6]
-arr1 = [4, 3, 2, 5, 3]
+arr1 = [4, 3, 2, 5, 99,-2]
+
 min_list = []
 max_list = []
 temp = 0
 min_temp = []
-for index, value in enumerate(arr):
-    if value < arr[index + 1]:
-        min_temp.append()
+
+for index, value in enumerate(arr1):
+    print(index)
+
+    if value > temp:
+        temp = value
+    else:
+        min_list.append(value)
+
+
+
+print(min_list)
+
