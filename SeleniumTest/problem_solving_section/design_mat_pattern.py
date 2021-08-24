d148a00528914adf890d9af25f44807200ca28d9
amits0003 <veddaily@gmail.com>
2021-08-24 10:40:19 +0530

    added new problem-solving codes

--- SeleniumTest/problem_solving_section/design_mat_pattern.py
+++ SeleniumTest/problem_solving_section/design_mat_pattern.py
@@ -1,23 +1,23 @@
+#!/usr/var/bin
 
 n = int(input('Enter the number of rows'))
-m = 3*int(n)
+m = 3 * int(n)
 res = []
 dot = '.'
 dot_stand = '.|.'
 letter = 'WELCOME'
-mid = n//2
+mid = n // 2
 counter = 1
 dec_counter = 1
-counter_iter2 = int((m-6)/3)
+counter_iter2 = int((m - 6) / 3)
+
+for i in range(1, n + 1):
+    print(str((counter * int((mid - 7) / 2))) + letter + str((counter * int((mid - 7) / 2))))
 
 
+# for i in range(1,n+1)
 
-for i in range(1,n+1):
-    print(str((counter*int((mid-7)/2))) + letter + str((counter*int((mid-7)/2))))
-#for i in range(1,n+1)
-
-
-#---------------------------------------------------
+# ---------------------------------------------------
 # print(5/2)
 # print(5//2)
 # print(5%2)
@@ -30,6 +30,4 @@ for i in range(1,n+1):
 #     res.append('\n')
 #
 # print(res)
-
-
 
